a88acc9b82ca79e7877502976e95299192892b8d
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-08 16:23:02 +0900

    新規登録ページのテストを追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,17 +2,8 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @user = users(:Keisuke)
-  end
-
   test "新規登録ページの取得" do
     get signup_path
-    assert_response :success
-  end
-
-  test "ユーザーページの取得" do
-    get @user
     assert_response :success
   end
 end
