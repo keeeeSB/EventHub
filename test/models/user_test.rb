52fd81c1ed1e64a7da6f419bfee556f2d4c8af6d
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-08 20:52:56 +0900

    Userモデルのバリデーションのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "nameカラムのバリデーションのテスト" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test "emailカラムのバリデーションのテスト" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
 end
