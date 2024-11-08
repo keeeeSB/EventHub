0ba07aa5599d98ad128931054ab6ebe0cb0979e4
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-08 16:42:28 +0900

    アップローダーをUserモデルに関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  mount_uploader :profile_image, ProfileImageUploader
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
