e53dbb1284979da8f5a97dafd5119dcdda3f44dc
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-09 10:29:35 +0900

    Categoryモデルにバリデーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_09_011815) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_09_012744) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
