4bbf16693df45877429ff2e8aa53de1f06d3477b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-09 21:23:16 +0900

    編集、削除ボタンの表示を選択

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -2,12 +2,7 @@ require "test_helper"
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
   test "イベント一覧ページの取得" do
-    get events_path
-    assert_response :success
-  end
-
-  test "イベント作成ページの取得" do
-    get new_user_event_path
+    get root_path
     assert_response :success
   end
 end
