4d3f24b5abd909a0166a59bf0e3258d03b48d022
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-09 12:13:36 +0900

    Categoriesコントローラーにアクションを追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,25 @@
 class CategoriesController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
   def new
+    @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "カテゴリーを作成しました。"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "カテゴリーを作成できませんでした。"
+      render :new
+    end
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
+ 
