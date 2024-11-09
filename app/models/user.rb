53e06cd612acd108be918146993ca049562fc9f4
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-09 22:32:58 +0900

    User, Eventモデルとの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-  has_many :events, dependent: :destroy
+  has_many :events,    dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   has_secure_password
   mount_uploader :profile_image, ProfileImageUploader
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name,     presence: true
+  validates :email,    presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 end
