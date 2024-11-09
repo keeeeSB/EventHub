ca8e4c4929cd250045c5fe617038d4a8c5a0bb2e
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-09 12:48:58 +0900

    Eventsコントローラーの雛形を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/show'
+  get 'events/edit'
   root "users#new"
   get  "/signup", to: "users#new"
   post "/signup", to: "users#create"
