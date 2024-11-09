e439443e5bda806753d047fc3650ce81efab9d27
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-09 13:18:41 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/new'
-  get 'events/show'
-  get 'events/edit'
-  root "users#new"
+  root "events#index"
   get  "/signup", to: "users#new"
   post "/signup", to: "users#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+  resources :users do
+    resources :events
+  end
   resources :categories
 end
