291cfeb5631478e59a4578393893acdd0dabfcf0
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-11 08:17:12 +0900

    Reviewモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_10_050105) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_10_231631) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -49,6 +49,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_10_050105) do
     t.index ["user_id"], name: "index_joins_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.integer "rating"
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_reviews_on_event_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -66,4 +77,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_10_050105) do
   add_foreign_key "favorites", "users"
   add_foreign_key "joins", "events"
   add_foreign_key "joins", "users"
+  add_foreign_key "reviews", "events"
+  add_foreign_key "reviews", "users"
 end
