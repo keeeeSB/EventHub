f1547f78509b46c649e5d32c5c0c066e6099fd4a
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-14 08:10:15 +0900

    Usersテーブルにadminカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_10_231631) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_13_230859) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_10_231631) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
