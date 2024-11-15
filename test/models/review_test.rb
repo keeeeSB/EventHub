0458aef81f4bb3cfac1a04728e8a234933472cc8
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-16 08:05:05 +0900

    Eventモデルのバリデーションのテストを追加

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class ReviewTest < ActiveSupport::TestCase
   
   def setup
-    @user = users(:Keisuke)
-    @event = events(:event)
+    @user =   users(:Keisuke)
+    @event =  events(:event)
     @review = Review.new(body: "レビュー内容", rating: 3,
                          user_id: @user.id, event_id: @event.id)
   end
