b9a8478b0d0b65cf8a37ea8ca1b0b5be41c0022b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-16 16:33:58 +0900

    参加イベント取り消しのテストを追加

--- test/integration/join_creation_test.rb
+++ test/integration/join_creation_test.rb
@@ -20,4 +20,24 @@ class JoinCreationTest < ActionDispatch::IntegrationTest
       follow_redirect!
     end
   end
+
+  test "参加イベントの取り消し" do
+    log_in_as(@user)
+    assert_redirected_to @user
+    follow_redirect!
+
+    assert_difference "Join.count", 1 do
+      get root_path
+      post user_event_joins_path(@user, @event), params: { join: { user_id: @user.id,
+                                                                   event_id: @event.id } }
+      assert_redirected_to root_path
+      follow_redirect!
+    end
+
+    assert_difference "Join.count", -1 do
+      delete user_event_join_path(@user, @event, Join.last)
+      assert_redirected_to root_path
+      follow_redirect!
+    end
+  end
 end
