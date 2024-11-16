79071f86e8c0df858884b8693bf800aace8b06cc
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-16 15:30:51 +0900

    イベント編集のテストを追加

--- test/integration/event_creation_test.rb
+++ test/integration/event_creation_test.rb
@@ -5,6 +5,7 @@ class EventCreationTest < ActionDispatch::IntegrationTest
   def setup
     @user     = users(:Keisuke)
     @category = categories(:sports)
+    @event    = events(:event)
   end
 
   test "イベント作成のテスト" do
@@ -25,5 +26,21 @@ class EventCreationTest < ActionDispatch::IntegrationTest
       follow_redirect!
       assert_match "テストイベント", response.body
     end
+  end
+
+  test "イベント編集のテスト" do
+    log_in_as(@user)
+    assert_redirected_to @user
+    follow_redirect!
+
+    get edit_user_event_path(@user, @event)
+    patch user_event_path(@user, @event), params: { event: { title: "編集したテストイベント",
+                                                              description: "テストイベントです。",
+                                                              start_time: 2.days.from_now,
+                                                              location: "カフェ",
+                                                              category_id: @category.id } }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_match "編集したテストイベント", response.body
   end
 end
