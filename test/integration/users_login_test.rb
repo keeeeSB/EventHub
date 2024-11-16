368d35eb2d7c96bbad9993cc6de0d5a8d5255c7a
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-16 14:54:48 +0900

    ログイン機能のテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,11 +2,18 @@ require "test_helper"
 
 class LoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:Keisuke)
+  end
+
   test "ログイン機能のテスト" do
     get login_path
-    post login_path, params: { session: { email: " ", password: " " } }
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
+    assert_response :success
+
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+
+    assert_redirected_to @user
+    follow_redirect!
   end
 end
